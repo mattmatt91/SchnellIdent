9779ff571ef950f3147dc6a432f3654b3ffe1da5
mattmatt91 <matthias_muhr@web.de>
2023-10-21 23:09:04 +0200

    _

--- frontend/src/components/MeasureComponent.js
+++ frontend/src/components/MeasureComponent.js
@@ -1,15 +1,15 @@
 import React, { useState } from 'react';
-import DataPlotComponent from './DataPlotComponent'; // Import the DataPlotComponent
+import DataPlotComponent from './DataPlotComponent';
+import { ClipLoader } from 'react-spinners'; // Import the ClipLoader spinner
 import './MeasureComponent.css';
 
 function MeasureComponent() {
   const [loading, setLoading] = useState(false);
-  const [data, setData] = useState(null); // To store the measurement data
-  const [params, setParams] = useState(null); // To store the measurement parameters
+  const [data, setData] = useState(null);
+  const [params, setParams] = useState(null);
 
   const handleRequest = async () => {
-    // Show loading animation
-    setLoading(true);
+    setLoading(true); // Show loading animation
 
     try {
       const response = await fetch('http://localhost:4000/measurement');
@@ -17,36 +17,34 @@ function MeasureComponent() {
 
       const measurementData = responseData.data;
       const measurementParams = responseData.params;
-      // Store the measurement data and parameters
+
       setData(measurementData);
       setParams(measurementParams);
 
-      // Hide loading animation when data is received
-      setLoading(false);
+      setLoading(false); // Hide loading animation when data is received
     } catch (error) {
-      console.error('Error:', error); // Handle errors
-
-      // Hide loading animation in case of an error
-      setLoading(false);
+      console.error('Error:', error);
+      setLoading(false); // Hide loading animation in case of an error
     }
   };
-
-
 
   return (
     <div className="measure">
       {loading ? (
-        <div className="loading-animation">Measuring...</div>
+        <div className="loading-animation">
+          <ClipLoader color={'red'} loading={loading} size={100} /> {/* Use ClipLoader */}
+        </div>
       ) : data ? (
         <>
           <DataPlotComponent data={data} />
           <button className="back-button" onClick={() => setData(null)}>
             Back to Measurement
           </button>
-         
-            <p>Explosive: {params.explosive ? <span style={{ color: 'red' }}>True</span> : <span style={{ color: 'green' }}>False</span>}</p>
-             
 
+          <div className="params">
+            <p>Explosive: {params.explosive ? <span style={{ color: 'red' }}>True</span> : <span style={{ color: 'green' }}>False</span>}</p>
+            {/* Add more parameter display here */}
+          </div>
         </>
       ) : (
         <button className="start-button" onClick={handleRequest}>
