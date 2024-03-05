9a80e715ee0f10a09c21e3d12621ea0d1517f195
mattmatt91 <matthias_muhr@web.de>
2024-03-05 21:46:10 +0100

    _

--- installer/install_rpi.sh
+++ installer/install_rpi.sh
@@ -20,4 +20,30 @@ scp install_docker_compose.sh $HOST_NAME@$HOST_IP:~/
 # Execute the Docker installation script on the remote host
 ssh $HOST_NAME@$HOST_IP 'bash ~/install_docker_compose.sh'
 
+bash ../deploy_to_rp
+
+
 # https://www.opendisplaycase.de/tutorials/raspberry-pi-display-waveshare-lcd-1024x600-hdmi-touch.html
+# sudo apt-get update && sudo apt-get -y upgrade
+# sudo apt-get install -y xserver-xorg-input-evdev
+# sudo nano /boot/firmware/config.txt
+#      dtparam=i2c_arm=on
+#      dtparam=spi=on
+#      enable_uart=1
+#      hdmi_group=2
+#      hdmi_mode=1
+#      hdmi_mode=87
+#      hdmi_cvt 1024 600 60 6 0 0 0
+#      dtoverlay=ads7846,cs=1,penirq=25,penirq_pull=2,speed=50000,keep_vref_on=0,swapxy=0,pmax=255,xohms=150,xmin=200,xmax=3900,ymin=200,ymax=3900
+
+# sudo nano /usr/share/X11/xorg.conf.d/99-calibration.conf
+#      zSection "InputClass"
+#        Identifier "calibration"
+#        MatchProduct "ADS7846 Touchscreen"
+#        Option "Calibration" "59 3985 3882 172"
+#        Option "SwapAxes" "1"
+#        Driver "evdev"
+#      EndSection
+#      sudo reboot
+
+# sudo apt-get install -y xinput-calibrator
